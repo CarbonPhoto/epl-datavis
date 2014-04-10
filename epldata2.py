5986d03cde36691ea857f67af0427b90a6a5350e
hsaleem <haji.saleem@mail.mcgill.ca>
2014-04-10 10:49:49 -0400

    epl stuff

---
+++ epldata2.py
@@ -0,0 +1,132 @@
+__author__ = 'SaleemSaddm'
+
+import pprint
+import  matplotlib.pyplot as plt
+
+
+fileepl = open("epl_data.txt", "r")
+
+k = fileepl.readlines()
+
+
+Teams = ["Arsenal", "Manchester City", "Manchester United", "Chelsea", "Tottenham Hotspur", "Everton", "Liverpool", "Newcastle United", "Leeds United", "Aston Villa", "Blackburn Rovers", "Nottingham Forest", "Norwich City" ]
+
+
+teamdict = {}
+
+years = []
+
+year = 0
+flag2 = 0
+for team in Teams:
+    flag = 0
+    temp = []
+    for line in k:
+            if "England" in line:
+                if flag == 1:
+                    rank = "23"
+                    temp.append(24-int(rank))
+                    flag = 0
+                year = line.split()[1][-2:]
+                if flag2 == 0:
+                    years.append(year)
+                flag = 1
+            if team in line:
+                rank = filter(lambda x: x.isdigit(), line.split()[0])
+                temp.append(24-int(rank))
+                flag = 0
+
+            if line.startswith("end"):
+                if flag == 1:
+                    rank = "23"
+                    temp.append(24-int(rank))
+
+    teamdict[team] = temp
+    flag2 =1
+
+
+
+ind = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+
+
+rankings = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "R", ]
+rankings.reverse()
+
+ax = plt.gca()
+fig = plt.gcf()
+ax.set_xticks(ind)
+ax.set_yticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+ax.set_xticklabels(years, fontsize=20)
+ax.set_yticklabels(rankings, fontsize=20)
+
+plt.grid(color='black', linestyle='--', linewidth=1)
+ax.set_ylim([0, 24.5])
+ax.set_xlim([0.5, 21.5])
+ax.set_axis_bgcolor("#bdbdbd")
+
+'''
+ranklist5 = teamdict["Arsenal"]
+p5 = plt.plot(ind, ranklist5, linewidth=20.0, color='#ed0206', marker="o", markersize = 25, markerfacecolor="w", markeredgewidth = 5, markeredgecolor = "#ed0206")
+
+ranklist = teamdict["Aston Villa"]
+p1 = plt.plot(ind, ranklist, linewidth=20.0, color='#94bee3', marker="o", markersize = 25, markerfacecolor="#78003a", markeredgewidth = 5, markeredgecolor = "#94bee3")
+
+ranklist10 = teamdict["Blackburn Rovers"]
+p10 = plt.plot(ind, ranklist10, linewidth=20.0, color='#009fde', marker="o", markersize = 25, markerfacecolor="#e00016", markeredgewidth = 5, markeredgecolor = "#009fde")
+
+ranklist7 = teamdict["Chelsea"]
+p7 = plt.plot(ind, ranklist7, linewidth=20.0, color='#044794', marker="o", markersize = 25, markerfacecolor="w", markeredgewidth = 5, markeredgecolor = "#044794")
+
+ranklist9 = teamdict["Leeds United"]
+p9 = plt.plot(ind, ranklist9, linewidth=20.0, color='w', marker="o", markersize = 25, markerfacecolor="#2c4d9c", markeredgewidth = 5, markeredgecolor = "w")
+
+ranklist2 = teamdict["Liverpool"]
+p2 = plt.plot(ind, ranklist2, linewidth=20.0, color='#cf0026', marker="o", markersize = 25, markerfacecolor="#00a193", markeredgewidth = 5, markeredgecolor = "#cf0026")
+
+ranklist3 = teamdict["Manchester United"]
+p3 = plt.plot(ind, ranklist3, linewidth=20.0, color='#d9020d', marker="o", markersize = 25, markerfacecolor="#000000", markeredgewidth = 5, markeredgecolor = "#d9020d")
+
+ranklist4 = teamdict["Manchester City"]
+p4 = plt.plot(ind, ranklist4, linewidth=20.0, color='#5ec0eb', marker="o", markersize = 25, markerfacecolor="w", markeredgewidth = 5, markeredgecolor = "#5ec0eb")
+
+ranklist8 = teamdict["Newcastle United"]
+p8 = plt.plot(ind, ranklist8, linewidth=20.0, color='black', marker="o", markersize = 25, markerfacecolor="w", markeredgewidth = 5, markeredgecolor = "black")
+
+ranklist11 = teamdict["Nottingham Forest"]
+p11 = plt.plot(ind, ranklist11, linewidth=20.0, color='#e33232', marker="o", markersize = 25, markerfacecolor="w", markeredgewidth = 5, markeredgecolor = "#e33232")
+
+ranklist12 = teamdict["Norwich City"]
+p12 = plt.plot(ind, ranklist12, linewidth=20.0, color='#00a64d', marker="o", markersize = 25, markerfacecolor="#ffee00", markeredgewidth = 5, markeredgecolor = "#00a64d")
+
+ranklist6 = teamdict["Tottenham Hotspur"]
+p6 = plt.plot(ind, ranklist6, linewidth=20.0, color='#001d57', marker="o", markersize = 25, markerfacecolor="w", markeredgewidth = 5, markeredgecolor = "#001d57")
+'''
+ranklist13 = teamdict["Everton"]
+p13 = plt.plot(ind, ranklist13, linewidth=20.0, color='#00509c', marker="o", markersize = 25, markerfacecolor="#ffdd1c", markeredgewidth = 5, markeredgecolor = "#00509c")
+
+'''
+plt.tick_params(\
+    axis='x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    labelbottom='off') # labels along the bottom edge are off
+
+
+plt.tick_params(\
+    axis='y',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    right='off',      # ticks along the bottom edge are off
+    left='off',         # ticks along the top edge are off
+    labelleft='off') # labels along the bottom edge are off
+
+'''
+
+
+
+
+
+plt.title("Everton", fontsize=30)
+fig.set_size_inches(40,10)
+plt.savefig("everton.png",bbox_inches='tight', dpi = 96, pad_inches=0.5)
+
---
+++ epldata_points.py
@@ -0,0 +1,56 @@
+__author__ = 'SaleemSaddm'
+
+import  matplotlib.pyplot as plt
+
+
+
+
+
+ind = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+first =  [84, 92, 89, 82, 75, 78, 79, 91, 80, 87, 83, 90, 95, 91, 89, 87, 90, 86, 80, 89, 89]
+second = [74, 84, 88, 78, 68, 77, 78, 73, 70, 80, 78, 79, 83, 83, 83, 85, 86, 85, 71, 89, 78]
+third =  [72, 77, 77, 71, 68, 65, 75, 69, 69, 77, 69, 75, 77, 82, 68, 83, 83, 75, 71, 70, 75]
+forth =  [71, 71, 74, 63, 68, 63, 67, 67, 68, 71, 67, 60, 61, 67, 68, 76, 72, 70, 68, 69, 73]
+
+
+ax = plt.gca()
+fig = plt.gcf()
+ax.set_xticks(ind)
+ax.set_yticks(list(xrange(0,98,2)))
+#ax.set_yticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+#ax.set_xticklabels(years)
+#ax.set_yticklabels(rankings, fontsize=20)
+
+#plt.grid(color='white', linestyle='-', linewidth=1)
+ax.set_ylim([58, 97])
+ax.set_xlim([0.5, 21.5])
+ax.set_axis_bgcolor("black")
+
+
+
+p1 = plt.plot(ind, first,  linewidth=20.0, color='#023858', marker="o", markersize = 25, markerfacecolor="#a6bddb", markeredgewidth = 5, markeredgecolor = "#023858")
+p2 = plt.plot(ind, second, linewidth=20.0, color='#800026', marker="o", markersize = 25, markerfacecolor="#feb24c", markeredgewidth = 5, markeredgecolor = "#800026")
+p3 = plt.plot(ind, third,  linewidth=20.0, color='#004529', marker="o", markersize = 25, markerfacecolor="#addd8e", markeredgewidth = 5, markeredgecolor = "#004529")
+p4 = plt.plot(ind, forth,  linewidth=20.0, color='#4d004b', marker="o", markersize = 25, markerfacecolor="#9ebcda", markeredgewidth = 5, markeredgecolor = "#4d004b")
+
+
+plt.tick_params(\
+    axis='x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    labelbottom='off') # labels along the bottom edge are off
+
+
+plt.tick_params(\
+    axis='y',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    right='off',      # ticks along the bottom edge are off
+    left='off',         # ticks along the top edge are off
+    labelleft='off') # labels along the bottom edge are off
+
+
+
+fig.set_size_inches(40,10)
+plt.savefig("test_pt3.png",bbox_inches='tight', dpi = 96, pad_inches=0, transparent = 'true')
+
